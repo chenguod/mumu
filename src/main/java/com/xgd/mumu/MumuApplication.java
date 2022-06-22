b1a15923c045b0289512455a36381f524ec51513
DESKTOP-4GMI6HI\Ray <1475313408@qq.com>
2022-06-22 21:47:47 +0800

    feature:初始化项目

---
+++ src/main/java/com/xgd/mumu/MumuApplication.java
@@ -0,0 +1,13 @@
+package com.xgd.mumu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MumuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xgd/mumu/MumuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgd.mumu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MumuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
